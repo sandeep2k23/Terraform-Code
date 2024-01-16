0fbdaef81998796bf9b9f7e7a4cd1813f8de80c5
sandeep23 <80738623+sandeep2k23@users.noreply.github.com>
2024-01-16 13:51:09 -0500

    Create main.tf

---
+++ EC2-ACA-S3/Account-B/main.tf
@@ -0,0 +1,67 @@
+locals {
+  tags = { ProjectName = var.project_name
+    ProjectMaker  = var.project_maker
+    MakerEmail    = var.contact_email
+    IAC_Framework = var.iac_framework
+    Account       = var.account_name
+  }
+}
+
+############## S3 Bucket #######################
+
+resource "aws_s3_bucket" "sc_s3_bucker" {
+  bucket = "sc-ec2-connect-s3"
+  tags = local.tags
+}
+
+############### IAM Policy ################
+
+resource "aws_iam_policy" "sc_s3_policy" {
+  name        = "ScEc2Policy"
+  description = "An ec2 role IAM policy"
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:*",
+            "s3-object-lambda:*"
+          ],
+          "Resource" : "*"
+        }
+      ]
+  })
+  tags = local.tags
+}
+
+
+################ IAM Role######################33
+
+resource "aws_iam_role" "sc_s3_role" {
+  name = "ScS3Role"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "Statement1",
+          "Effect" : "Allow",
+          "Principal" : {
+            "AWS" : "arn:aws:iam::889142710491:root"
+          },
+          "Action" : "sts:AssumeRole"
+        }
+      ]
+  })
+  tags = local.tags
+}
+
+############IAM Role Policy Attachment############
+resource "aws_iam_role_policy_attachment" "sc_s3_attachment" {
+  policy_arn = aws_iam_policy.sc_s3_policy.arn
+  role       = aws_iam_role.sc_s3_role.name
+}
