5b481981b209725352183f64fea52af1eacdf652
sandeep23 <80738623+sandeep2k23@users.noreply.github.com>
2024-01-18 13:23:02 -0500

    Create detector.tf

---
+++ GuardDuty/detector.tf
@@ -0,0 +1,21 @@
+resource "aws_guardduty_detector" "MyDetector" {
+  enable = true
+
+  datasources {
+    s3_logs {
+      enable = true
+    }
+    kubernetes {
+      audit_logs {
+        enable = false
+      }
+    }
+    malware_protection {
+      scan_ec2_instance_with_findings {
+        ebs_volumes {
+          enable = false
+        }
+      }
+    }
+  }
+}
